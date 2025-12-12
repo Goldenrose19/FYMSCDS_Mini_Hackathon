12b5e694e21e5f11fde13892c647594c280b84ab
Goldenrose19 <78873462+Goldenrose19@users.noreply.github.com>
2025-12-12 19:00:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('To my dear Prachi')
+st.title('To my dear Prachi 💗')
 
 st.write('Like the redness of the East, the first ray of the sun, You are just like that, a reason for joy. Hidden in your name is a new beginning, Your friendship is like a pleasant morning breeze.May this whole world light up with your smile, May you take step after step on the path of your dreams. Prachi, your existence is like a drop of dew, Just your presence gives the mind a rest, a shelter.Keep smiling always, keep blooming like a flower, Keep shining like a star in the sky of life.All the best for your tommorows exam!!')
